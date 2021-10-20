4fdca9c86c5959014639808280bbebb54721b4de
khyati-l <khyati.lingerker@education.nsw.gov.au>
2021-10-21 10:01:02 +1100

    first commit

---
+++ 02GoodbyeWorld.py
@@ -0,0 +1,11 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    02GoodbyeWorld                              |")
+print("|    Name : Khyati                               |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+print("Hello World")
+
+input("------> Program paused - press enter to continue")
+print("Goodbye World")
