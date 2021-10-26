91ab5d2a0bff6d65be156bed7819d186c0f4a350
khyati-l <khyati.lingerker@education.nsw.gov.au>
2021-10-27 00:21:35 +1100

    04

---
+++ 04GoodTeacher.py
@@ -0,0 +1,14 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Khyati                               |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+print("Hello")
+
+name = input("Teacher name(try Mr Horan) " )
+if name == "Mr Horan":
+    print ("You are lucky, he is a great teacher.")
+else:
+    print (name + " is an ok teacher")
