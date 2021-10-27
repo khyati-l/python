c195798d243cd332242cdcdeffdb679b85cf02d1
khyati-l <khyati.lingerker@education.nsw.gov.au>
2021-10-28 10:34:13 +1100

    05

--- 05forLoop.py
+++ 05forLoop.py
@@ -5,3 +5,6 @@ print("|    Name : Khyati                               |")
 print("|    Version : 01                                |")
 print("|                                                |")
 print(" ------------------------------------------------")
+
+for x in range(1,500):  
+    print(x)
