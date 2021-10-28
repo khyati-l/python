e033c75fa60a95426cfeb955d76b9bb3d3b361d7
khyati-l <khyati.lingerker@education.nsw.gov.au>
2021-10-28 17:28:43 +1100

    06

---
+++ 06whileLoop.py
@@ -0,0 +1,21 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Khyati                               |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+
+x=0
+while x == 0:
+    subject = input("What is the name of this subject ")
+    if subject == "IST":
+        print(" ")
+        print(" ")
+        print("Congratulations!!")
+        print(" ")
+        print(" ")
+        print(" ")
+        break
+    else:
+        print("Not Correct - try again")
